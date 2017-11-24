8ee3dd4c11d431191e396c0cedd40f3a7eb4fdd2
Jesús Malo Poyatos <jmalopoy@opentext.com>
2017-11-24 09:44:05 +0100

    Enforcing encoding to UTF-8

--- src/main/java/com/tguzik/m2u/application/Main.java
+++ src/main/java/com/tguzik/m2u/application/Main.java
@@ -1,6 +1,7 @@
 package com.tguzik.m2u.application;
 
 import java.io.*;
+import java.nio.charset.Charset;
 
 import com.tguzik.m2u.data.JtlToJunitConverter;
 import com.tguzik.m2u.data.jtl.TestResults;
@@ -21,13 +22,16 @@ public class Main {
     }
 
     public void run( ProgramOptions programOptions ) throws IOException {
-        try ( InputStreamReader input = new FileReader( programOptions.getInputFileName() );
-              OutputStreamWriter output = new FileWriter( programOptions.getOutputFileName() ) ) {
+        try ( InputStreamReader input = new InputStreamReader( new FileInputStream( programOptions.getInputFileName() ),
+                                                               Charset.forName("UTF-8").newDecoder() );
+              OutputStreamWriter output = new OutputStreamWriter( new FileOutputStream( programOptions.getOutputFileName() ),
+                                                                  Charset.forName("UTF-8").newEncoder() ) ) {
 
             TestResults jmeterResults = new JmeterXmlConverter().fromXML( input );
             TestSuites junitResults = new JtlToJunitConverter().apply( programOptions.getTestSuiteName(),
                                                                        jmeterResults );
 
+            output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
             new JunitXmlConverter().toXML( junitResults, output );
         }
     }
