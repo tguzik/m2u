4b8f6b154e0518345b7e622ec832992992f45ab5
Tomasz Guzik <tomek@tguzik.com>
2018-03-30 00:09:53 +0200

    Skip empty/missing components of the resulting class name. Improve code style in JtlToJunitConverter a bit.

--- src/main/java/com/tguzik/m2u/data/JtlToJunitConverter.java
+++ src/main/java/com/tguzik/m2u/data/JtlToJunitConverter.java
@@ -4,36 +4,42 @@ import static com.tguzik.util.CollectionUtils.safe;
 
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.tguzik.m2u.constants.M2UConstants;
 import com.tguzik.m2u.data.jtl.AssertionResult;
 import com.tguzik.m2u.data.jtl.BaseSample;
 import com.tguzik.m2u.data.jtl.HttpSample;
 import com.tguzik.m2u.data.jtl.TestResults;
+import com.tguzik.m2u.data.junit.Error;
 import com.tguzik.m2u.data.junit.Failure;
 import com.tguzik.m2u.data.junit.TestCase;
 import com.tguzik.m2u.data.junit.TestSuite;
 import com.tguzik.m2u.data.junit.TestSuites;
 import com.tguzik.m2u.params.ControlParams;
+import org.apache.commons.lang3.StringUtils;
 
 public class JtlToJunitConverter implements BiFunction<String, TestResults, TestSuites> {
 
     @Override
     public TestSuites apply( String testSuiteName, TestResults input ) {
-        final TestSuites suites = new TestSuites();
+        final String filter = ControlParams.getParam( M2UConstants.JUNIT_FILTER_SWITCH_NAME );
         final TestSuite singleSuite = new TestSuite();
+        final TestSuites suites = new TestSuites();
+        final Predicate<BaseSample> p;
 
         suites.setTestGroupName( testSuiteName );
         singleSuite.setName( testSuiteName );
 
-        String filter = ControlParams.getParam( M2UConstants.JUNIT_FILTER_SWITCH_NAME );
-        Predicate<BaseSample> p = a -> true;
-        if ( filter != null && filter.equalsIgnoreCase( M2UConstants.TRUE ) ) {
+        if ( StringUtils.equalsIgnoreCase( M2UConstants.TRUE, filter ) ) {
             p = a -> a.getLabel().contains( M2UConstants.JUNIT_DECORATOR );
+        }
+        else {
+            p = x -> true;
         }
 
         safe( input.getSamples() ).stream().filter( p ).map( this::convertSample ).forEach( singleSuite::addTestCase );
-
         safe( input.getHttpSamples() ).stream()
                                       .filter( p )
                                       .map( this::convertHttpSample )
@@ -45,14 +51,17 @@ public class JtlToJunitConverter implements BiFunction<String, TestResults, Test
     }
 
     private TestCase convertSample( BaseSample input ) {
-        final String threadPlusLabel = input.getThreadName() + " - " + input.getLabel();
-        TestCase tc = new TestCase();
+        final String filter = ControlParams.getParam( M2UConstants.JUNIT_FILTER_SWITCH_NAME );
+        final String threadPlusLabel = Stream.of( StringUtils.trimToEmpty( input.getThreadName() ),
+                                                  StringUtils.trimToEmpty( input.getLabel() ) )
+                                             .filter( StringUtils::isNotEmpty )
+                                             .collect( Collectors.joining( " - " ) );
+        final TestCase tc = new TestCase();
 
         tc.setAssertions( safe( input.getAssertionResults() ).size() );
         tc.setClassname( threadPlusLabel );
 
-        String filter = ControlParams.getParam( M2UConstants.JUNIT_FILTER_SWITCH_NAME );
-        if ( filter != null && filter.equalsIgnoreCase( M2UConstants.TRUE ) ) {
+        if ( StringUtils.equalsIgnoreCase( M2UConstants.TRUE, filter ) ) {
             tc.setTestName( input.getLabel().replace( M2UConstants.JUNIT_DECORATOR, M2UConstants.BLANK ) );
         }
         else {
@@ -65,12 +74,12 @@ public class JtlToJunitConverter implements BiFunction<String, TestResults, Test
         safe( input.getAssertionResults() ).stream()
                                            .filter( AssertionResult::isError )
                                            .map( this::convertErrorAssertion )
-                                           .forEach( x -> tc.getErrors().add( x ) );
+                                           .forEach( tc.getErrors()::add );
 
         safe( input.getAssertionResults() ).stream()
                                            .filter( AssertionResult::isFailure )
                                            .map( this::convertFailureAssertion )
-                                           .forEach( x -> tc.getFailures().add( x ) );
+                                           .forEach( tc.getFailures()::add );
 
         return tc;
     }
@@ -80,7 +89,7 @@ public class JtlToJunitConverter implements BiFunction<String, TestResults, Test
     }
 
     private Failure convertFailureAssertion( AssertionResult ar ) {
-        Failure failure = new Failure();
+        final Failure failure = new Failure();
 
         failure.setMessage( ar.getFailureMessage() );
         failure.setType( ar.getName() );
@@ -88,8 +97,8 @@ public class JtlToJunitConverter implements BiFunction<String, TestResults, Test
         return failure;
     }
 
-    private com.tguzik.m2u.data.junit.Error convertErrorAssertion( AssertionResult ar ) {
-        com.tguzik.m2u.data.junit.Error error = new com.tguzik.m2u.data.junit.Error();
+    private Error convertErrorAssertion( AssertionResult ar ) {
+        final Error error = new Error();
 
         error.setMessage( ar.getFailureMessage() );
         error.setType( ar.getName() );
